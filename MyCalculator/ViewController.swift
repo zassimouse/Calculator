961e0b917b3fe128f7d7a9d8bba002deadf5ae10
Denis Haritonenko <denharitonenko@gmail.com>
2024-06-13 17:40:44 +0300

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by Denis Haritonenko on 13.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
