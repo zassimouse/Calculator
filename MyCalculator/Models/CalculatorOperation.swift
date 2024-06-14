865700c85e0dedbb8f4cc64b99a7621cc67577ba
Denis Haritonenko <denharitonenko@gmail.com>
2024-06-14 11:36:28 +0300

    fixed

--- MyCalculator/Models/CalculatorOperation.swift
+++ MyCalculator/Models/CalculatorOperation.swift
@@ -2,28 +2,7 @@
 //  CalculatorOperation.swift
 //  MyCalculator
 //
-//  Created by Denis Haritonenko on 13.06.24.
+//  Created by Denis Haritonenko on 14.06.24.
 //
 
 import Foundation
-
-enum CalculatorOperation {
-    
-    case add
-    case subtract
-    case multiply
-    case divide
-    
-    var title: String {
-        switch self {
-        case .add:
-            return "+"
-        case .subtract:
-            return "-"
-        case .multiply:
-            return "×"
-        case .divide:
-            return "÷"
-        }
-    }
-}
